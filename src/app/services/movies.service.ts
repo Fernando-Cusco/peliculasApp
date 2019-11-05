20ed8c0bdc23754eb8793696bf683f1269f36db1
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-11-05 11:36:40 -0500

    [add] interface to Movies

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MovieDB } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class MoviesService {
 
   getFeature() {
     // tslint:disable-next-line: max-line-length
-    return this.http.get(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2019-11-03&api_key=2d5ed9d0742928ec0f23a38aa11a7696&language=es&include_image_language=es`);
+    return this.http.get<MovieDB>(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2019-11-03&api_key=2d5ed9d0742928ec0f23a38aa11a7696&language=es&include_image_language=es`);
 
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
+import { Pelicula } from '../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit{
+export class Tab1Page implements OnInit {
 
+  peliculasRecientes: Pelicula[] = [];
   constructor(private movieService: MoviesService) {}
 
   ngOnInit() {
-    this.movieService.getFeature().subscribe( response => {
-      console.log(response);
+    this.movieService.getFeature()
+        .subscribe( response => {
+          console.log(response);
+          this.peliculasRecientes = response.results;
     });
   }
 }
