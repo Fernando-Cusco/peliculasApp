dc19b75d44a519be160ce6ae6e503cef1b876e17
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-11-10 18:49:09 -0500

    [add] tunning urls

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 import { MovieDB } from '../interfaces/interfaces';
+
+const URL = environment.url;
+const apiKey = environment.apiKey;
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +13,28 @@ export class MoviesService {
 
   constructor(private http: HttpClient) { }
 
+  private ejecutarQuery<T>(query: string) {
+    query = URL + query;
+    query += `&api_key=${apiKey}&language=es&include_image_language=es`;
+    return this.http.get<T>(query);
+  }
+
   getFeature() {
+    const hoy = new Date();
+    const ultimoDia = new Date(hoy.getFullYear(), hoy.getMonth() + 1, 0).getDate();
+    let mes = hoy.getMonth() + 1;
+    let mesString;
+
+    if ( mes < 10 ) {
+      mesString = '0' + mes;
+    } else {
+      mesString = mes;
+    }
+    const inicio = `${ hoy.getFullYear()}-${mesString}-01`;
+    const fin = `${ hoy.getFullYear()}-${mesString}-${ultimoDia}`;
+
     // tslint:disable-next-line: max-line-length
-    return this.http.get<MovieDB>(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2019-11-03&api_key=2d5ed9d0742928ec0f23a38aa11a7696&language=es&include_image_language=es`);
+    return this.ejecutarQuery<MovieDB>(`/discover/movie?primary_release_date.gte=${ inicio }&primary_release_date.lte=${ fin }`);
 
   }
 }
